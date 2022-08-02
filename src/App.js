39d184a1e05a1cfec83416939d103021335b41f0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-08-03 02:10:09 +0300

    add the UI for the calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App" >
-      
-    </div>
-  );
+import Calculator from './components/Calculator';
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div className="calculator_container">
+        <div className="result_div">0</div>
+        <div className="first_row">
+          <div className="button">AC</div>
+          <div className="button">+/-</div>
+          <div className="button">%</div>
+          <div className="button operation_button">+</div>
+        </div>
+        <div className="first_row">
+          <div className="button">7</div>
+          <div className="button">8</div>
+          <div className="button">9</div>
+          <div className="button operation_button">x</div>
+        </div>
+        <div className="first_row">
+          <div className="button">4</div>
+          <div className="button">5</div>
+          <div className="button">6</div>
+          <div className="button operation_button">-</div>
+        </div>
+        <div className="first_row">
+          <div className="button">1</div>
+          <div className="button">2</div>
+          <div className="button">3</div>
+          <div className="button operation_button">+</div>
+        </div>
+        <div className="first_row">
+          <div className="button zero_button">0</div>
+          <div className="button">.</div>
+          <div className="button operation_button">=</div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Calculator;
