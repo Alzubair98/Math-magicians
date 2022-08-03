310c8dbbfb3f2fba0583c5baa0b80672826d5529
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-08-03 22:27:37 +0300

    Change from div to button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,58 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+    this.result = this.result.bind(this);
+  }
+
+  result(e) {
+    const { obj } = this.state;
+    const newObject = calculate(obj, e.target.textContent);
+    this.setState({ obj: newObject });
   }
 
   render() {
+    const obj = this.state;
     return (
       <div className="calculator_container">
-        <div className="result_div">0</div>
+        <div className="result_div">{obj.next || obj.total || 0}</div>
         <div className="first_row">
-          <div className="button">AC</div>
-          <div className="button">+/-</div>
-          <div className="button">%</div>
-          <div className="button operation_button">+</div>
+          <button type="button" className="button">AC</button>
+          <button type="button" className="button">+/-</button>
+          <button type="button" className="button">%</button>
+          <button type="button" className="button operation_button">+</button>
         </div>
         <div className="first_row">
-          <div className="button">7</div>
-          <div className="button">8</div>
-          <div className="button">9</div>
-          <div className="button operation_button">x</div>
+          <button type="button" className="button">7</button>
+          <button type="button" className="button">8</button>
+          <button type="button" className="button">9</button>
+          <button type="button" className="button operation_button">x</button>
         </div>
         <div className="first_row">
-          <div className="button">4</div>
-          <div className="button">5</div>
-          <div className="button">6</div>
-          <div className="button operation_button">-</div>
+          <button type="button" className="button">4</button>
+          <button type="button" className="button">5</button>
+          <button type="button" className="button">6</button>
+          <button type="button" className="button operation_button">-</button>
         </div>
         <div className="first_row">
-          <div className="button">1</div>
-          <div className="button">2</div>
-          <div className="button">3</div>
-          <div className="button operation_button">+</div>
+          <button type="button" className="button">1</button>
+          <button type="button" className="button">2</button>
+          <button type="button" className="button">3</button>
+          <button type="button" className="button operation_button">+</button>
         </div>
         <div className="first_row">
-          <div className="button zero_button">0</div>
-          <div className="button">.</div>
-          <div className="button operation_button">=</div>
+          <button type="button" className="button zero_button">0</button>
+          <button type="button" className="button">.</button>
+          <button type="button" className="button operation_button">=</button>
         </div>
       </div>
 
