1f697936bcb53ddbddc5b17209b27f2f65e45c4a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-08-09 23:00:18 +0300

    add to nav bar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-  <div>
-    <h1>Math Magicians</h1>
-  </div>
-);
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 1,
+      path: '/Calculator',
+      text: 'Calculator',
+    },
+    {
+      id: 1,
+      path: '/Quote',
+      text: 'Quote',
+    },
+  ];
+
+  return (
+    <div>
+      {links.map((link) => (
+        <li className="navbar-item" key={link.id}>
+          <NavLink to={link.path} className="active-link">{link.text}</NavLink>
+        </li>
+      ))}
+      <h1>Math Magicians</h1>
+    </div>
+  );
+};
 
 export default Navbar;
